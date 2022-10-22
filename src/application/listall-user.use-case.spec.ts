32747cdc4a7f1640e5026c02619c09662787ab5f
ItaloCobains <monokatchaleger@gmail.com>
2022-10-21 22:22:09 -0300

    feat: listAll UseCases test

--- src/application/listall-user.use-case.spec.ts
+++ src/application/listall-user.use-case.spec.ts
@@ -1,3 +1,11 @@
+import { UserInMemoryRepository } from '../infra/db/user-in-memory.repository';
+import { ListAllUserUseCase } from './listall-user.use-case';
+
 describe('listall UserCase Test', () => {
-  it('', () => {});
+  it('should list all users', async () => {
+    const repo = new UserInMemoryRepository();
+    const listAllUserUseCase = new ListAllUserUseCase(repo);
+    const result = await listAllUserUseCase.execute();
+    expect(result).toHaveLength(0);
+  });
 });
